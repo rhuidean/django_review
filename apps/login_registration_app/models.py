e2be615cc2470d556ae63288cf4bbb47edfcfabe
Jason <rhuidean06@gmail.com>
2017-06-25 16:41:17 -0500

    add post model

--- apps/login_registration_app/models.py
+++ apps/login_registration_app/models.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import re, bcrypt
 
+### Create the models and add additional method to the built in model methods
 ''' Spelling!!! '''
+
 class User(models.Model):
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
@@ -11,4 +14,31 @@ class User(models.Model):
 	password = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+	objects = UserManager()
+
+	def __str__(self):
+		string_output = "first_name: {} last_name: {} email: {} password: {}"
+
+		return string_output.format(
+			self.first_name,
+			self.last_name,
+			self.email,
+			self.password,
+		)
+### one to many relationship: post->user
+class Post(models.Model):
+	post = models.CharField(max_length=255)
+	user = models.ForeignKey(User, related_name="posts")
+	likes = models.ManyToManyField(User, related_name="posts_liked")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	objects = PostManager()
+
+
+
+
+
+
+
+
 
