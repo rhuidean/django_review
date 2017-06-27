4eaa3533dbc39d7732731ce6376c746bf745b129
Jason <rhuidean06@gmail.com>
2017-06-26 20:52:59 -0500

    add create user view

--- apps/login_registration_app/views.py
+++ apps/login_registration_app/views.py
@@ -8,6 +8,19 @@ from .models import *
 def index(request):
 	return render(request,'login_registration_app/index.html')
 
+def createUser(request):
+	if request.method != 'POST':
+		return redirect('/')
+	attempt = User.objects.validateUser(request.POST)
+	if attempt['status'] == True:
+		user = User.objects.createUser(request.POST)
+		request.session['user_id'] = user.id
+		return redirect('/books')
+	else:
+		for error in attempt['errors']:
+			messages.add_message(request, messages.ERROR, error, extra_tags="registration")
+			return redirect('/')
+
 def showBook(request, id):
 	book = Book.objects.filter(id=id).first()
 	context = {
