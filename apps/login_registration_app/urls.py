acbcc11a6e602d9f22f991f62280178d3109b323
Jason <rhuidean06@gmail.com>
2017-06-26 20:56:02 -0500

    add views.createUser to urls

--- apps/login_registration_app/urls.py
+++ apps/login_registration_app/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 '''import methods '''
 
+
+### Similar to node express server routes
 urlpatterns = [
 	url(r'^$',views.index),
+	url(r'^$users$',views.createUser),
 ]
