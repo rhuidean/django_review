4ce033c11bda7c9beeb80835a57b8dd8ded175de
Jason <rhuidean06@gmail.com>
2017-06-26 20:48:08 -0500

    add show_book.html

--- apps/login_registration_app/views.py
+++ apps/login_registration_app/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render,redirect
+### one-time flash messages
 from django.contrib import messages
 from django.db.models import Count
 from .models import *
