567260a72f9b7fda59a4b7c953c6daa4cfcabfa7
Jason <rhuidean06@gmail.com>
2017-06-26 20:50:41 -0500

    add show book view

--- apps/login_registration_app/views.py
+++ apps/login_registration_app/views.py
@@ -7,3 +7,12 @@ from .models import *
 
 def index(request):
 	return render(request,'login_registration_app/index.html')
+
+def showBook(request, id):
+	book = Book.objects.filter(id=id).first()
+	context = {
+		'book': book,
+		'reviews': book.reviews.select_related('user').all(),
+		'current_user': current_user(request),
+	}
+	return render(request, 'main/show_book.html', context)
